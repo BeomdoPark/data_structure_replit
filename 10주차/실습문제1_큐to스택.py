ed2a724cc589c72623554f82a6c5c7ddccb0ff10
BeomdoPark <127182331+BeomdoPark@users.noreply.github.com>
2024-05-13 08:15:49 +0000

    add 실습문제1

--- 10주차/실습문제1_큐to스택.py
+++ 10주차/실습문제1_큐to스택.py
@@ -1,3 +1,4 @@
+#queue 2개를 이용한 Stack 구현
 class myStack():
 
     def __init__(self):
@@ -8,7 +9,22 @@ class myStack():
         return self.main_queue.empty()
 
     def top(self):
-        pass
+        #main_queue가 한개 남을 때까지 뽑기.
+        while (self.main_queue.front + 1 != self.main_queue.rear):
+            data = self.main_queue.dequeue()
+            #뽑은값은 sub_queue에 넣기
+            self.sub_queue.enqueue(data)
+        #하나 남은 값을 top_data에 저장
+        top_data = self.main_queue.peek()
+        #서브큐에 넣기
+        data = self.main_queue.dequeue()
+        self.sub_queue.enqueue(data)
+
+        #sub_queue에 있는 값들을 다시 main_queue에 넣기
+        while (not self.sub_queue.empty()):
+            data = self.sub_queue.dequeue()
+            self.main_queue.enqueue(data)
+        return top_data
 
     #메인큐에 들어온 순서대로 넣기
     def push(self, data):
@@ -16,12 +32,15 @@ class myStack():
 
     def pop(self):
         #main_queue가 한개 남을 때까지 뽑기.
-        while (self.main_queue.front + 1 == self.main_queue.rear):
+        while (self.main_queue.front + 1 != self.main_queue.rear):
             data = self.main_queue.dequeue()
             #뽑은값은 sub_queue에 넣기
             self.sub_queue.enqueue(data)
-        #하나 남은 값이 pop하려는 데이터임.
-        pop_data = self.main_queue.dequeue()
+
+        #하나 남은 값을 pop_data에 저장 후 버리기
+        pop_data = self.main_queue.peek()
+        self.main_queue.dequeue()
+
         #sub_queue에 있는 값들을 다시 main_queue에 넣기
         while (not self.sub_queue.empty()):
             data = self.sub_queue.dequeue()
@@ -36,12 +55,28 @@ class queue():
         self.queue = [None for _ in range(self.SIZE)]
         self.front = self.rear = -1
 
+    def isQueueFull(self):
+        if (self.rear != self.SIZE - 1):
+            return False
+        elif (self.rear == self.SIZE - 1) and (self.front == -1):
+            return True
+        else:
+            for i in range(self.front + 1, self.SIZE):
+                self.queue[i - 1] = self.queue[i]
+                self.queue[i] = None
+            self.front -= 1
+            self.rear -= 1
+            return False
+
+    def peek(self):
+        return self.queue[self.front + 1]
+
     def empty(self):
         return self.front == self.rear
 
     def enqueue(self, data):
-        if (self.rear == self.SIZE - 1):
-            print("큐가 꽉 찼습니다.")
+        if (self.isQueueFull()):
+            print("* 큐가 꽉 찼습니다. *")
         else:
             self.rear += 1
             self.queue[self.rear] = data
@@ -49,7 +84,7 @@ class queue():
     def dequeue(self):
         # if (self.front == self.rear):  #isQueueEmpty()
         #     print("큐가 비었습니다.")
-        #     return None
+        #     return False
         self.front += 1
         data = self.queue[self.front]
         self.queue[self.front] = None
@@ -58,10 +93,33 @@ class queue():
 
 if __name__ == '__main__':
     my_stack = myStack()
-    return_list = []
-    return_list.append(my_stack.push(1))
-    return_list.append(my_stack.push(2))
-    # return_list.append(my_stack.top())
-    return_list.append(my_stack.pop())
-    return_list.append(my_stack.empty())
-    print(return_list)
+    result_list = []
+    select = input("push(1) / pop(2) / top(3) / empty(4) / exit(X) :")
+    while (select.lower() != 'x'):
+        if (select == '1'):
+            data = input("push할 데이터 입력 : ")
+            result_list.append(my_stack.push(data))
+        elif (select == '2'):
+            if (my_stack.empty()):
+                print("큐가 비어있습니다.")
+            else:
+                result_list.append(my_stack.pop())
+        elif (select == '3'):
+            if (my_stack.empty()):
+                print("큐가 비어있습니다.")
+            else:
+                result_list.append(my_stack.top())
+        elif (select == '4'):
+            result_list.append(my_stack.empty())
+        else:
+            print("* 잘못된 입력입니다. *")
+
+        print("현재 queue = ", my_stack.main_queue.queue, end="\n\n")
+        select = input("push(1) / pop(2) / top(3) / empty(4) / exit(X) :")
+
+    # result_list.append(my_stack.push(1))
+    # result_list.append(my_stack.push(2))
+    # result_list.append(my_stack.top())
+    # result_list.append(my_stack.pop())
+    # result_list.append(my_stack.empty())
+    print(result_list)
